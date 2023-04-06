fcac6a897530408e62747af59d4f07d061de1e76
Navojith <navojith22@gmail.com>
2023-04-06 14:18:45 +0530

    logic done

--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -2,10 +2,60 @@ package com.example.scorekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mScoreText1: TextView
+    private lateinit var mScoreText2: TextView
+    var mScore1 : Int = 0
+    var mScore2 : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mScoreText1 = findViewById<TextView>(R.id.score_1)
+        mScoreText2 = findViewById<TextView>(R.id.score_2)
+
+        mScore1 = mScoreText1?.text.toString().toInt()
+        mScore2 = mScoreText2?.text.toString().toInt()
+    }
+
+    fun decreaseScore(view: View) {
+
+        when(view.id) {
+            R.id.decreaseTeam1 -> {
+                mScore1--;
+                mScoreText1.text= mScore1.toString()
+            }
+
+            R.id.decreaseTeam2 -> {
+                mScore2--
+                mScoreText2.text= mScore2.toString()
+            }
+
+            else -> {
+                println("Invalid")
+            }
+        }
+    }
+
+    fun increaseScore(view: View) {
+        when(view.id) {
+            R.id.increaseTeam1 -> {
+                mScore1++;
+                mScoreText1.text = mScore1.toString()
+            }
+
+            R.id.increaseTeam2 -> {
+                mScore2++
+                mScoreText2.text= mScore2.toString()
+            }
+
+            else ->  {
+                println("Invalid")
+            }
+        }
     }
 }
